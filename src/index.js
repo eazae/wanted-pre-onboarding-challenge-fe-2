579fe82f27195b861f2c9cb3f5ae67758843a888
eazae <xynezae@gmail.com>
2022-09-02 13:10:55 +0900

    feat: jsdoc with template

---
+++ src/index.js
@@ -0,0 +1,45 @@
+/**
+ * Represents a Todos.
+ * @typedef Todo
+ * @type {Object}
+ * @property {string} id - To do id.
+ * @property {string} contents - To do Contents.
+ * @property {boolean} isCompleted - To do Completed.
+ * @property {string} categorys - To do categorys.
+ * @property {string} tags - To do tags.
+ */
+
+/**
+ * Create todo.
+ * @function createTodo
+ * @param {string} id - To do id.
+ * @param {string} contents - To do Contents.
+ * @param {string} categorys - To do categorys.
+ * @param {string} tags - To do tags.
+ */
+function CreateTodo(id, contents, categorys, tags) { }
+
+/**
+ * Read todo.
+ * @function readTodo
+ * @param {string} id - To do id.
+ */
+function ReadTodo(id) { }
+
+/**
+ * Update todo.
+ * @function updateTodo
+ * @param {string} id - To do id.
+ * @param {string} contents - To do Contents.
+ * @param {boolean} isCompleted - To do Completed.
+ * @param {string} categorys - To do categorys.
+ * @param {string} tags - To do tags.
+ */
+function UpdateTodo(id, contents, isCompleted, categorys, tags) { }
+
+/**
+ * Update Delete.
+ * @param {string} id - To do id.
+ * @function deleteTodo
+ */
+function DeleteTodo(id) { }
